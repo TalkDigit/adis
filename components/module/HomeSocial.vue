4a453e2932b9d184097621c5d696a1d290cb4db9
Hilal MORGÜL <63613290+hilalmorgul@users.noreply.github.com>
2025-03-24 23:39:48 +0300

    s

--- components/module/HomeSocial.vue
+++ components/module/HomeSocial.vue
@@ -7,10 +7,24 @@
             </div>
         </div>
         <div>
-            <div class="circle-background"></div>
+            <div class="lottie">
+            <lottie-player src="/assets/json/circle.json" background="transparent" speed="1" style="width: 100%; max-width:1920px; height: 1920px;" loop autoplay></lottie-player>
+        </div>
+            <ModuleSocialSlider/>
         </div>
     </section>
 </template>
 <style>
-
+.lottie{
+    scale:1.6;
+    z-index:-1;
+    position:relative;
+    margin-top:100px
+}
 </style>
+
+<script setup>
+      useHead({
+  script: [ { src: 'https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js' } ]
+})
+</script>
