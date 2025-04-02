07588ad6f3390004441f4beb880aa97aaafbd6e5
Hilal MORGÜL <63613290+hilalmorgul@users.noreply.github.com>
2025-04-02 13:41:41 +0300

    ss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+
+
+  app : {
+    head: {
+      link: [
+         { rel: 'icon', type: 'image/png', href: "/assets/images/favicon.png" },
+      ]
+   }
+  },
+
+
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   router: {
